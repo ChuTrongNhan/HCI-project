ca5b2d90a5be8a9ae9880bc2d8c15661ba1604f1
ChuTrongNhan <44189075+ChuTrongNhan@users.noreply.github.com>
2020-07-02 15:40:46 +0700

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/chutrongnhan.github.io/hci-final-build/" : "/",
-};
-module.exports = {
-  transpileDependencies: ["x5-gmaps"],
-};
